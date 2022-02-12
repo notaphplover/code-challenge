4204a3d9f9cf869218db29757d5f74a94fdcb400
notaphplover <roberto.pintos.lopez@gmail.com>
2022-02-12 22:54:50 +0100

    refactor: add DotEnvLoader

---
+++ src/env/integration/dotenv/DotEnvLoader.spec.ts
@@ -0,0 +1,79 @@
+jest.mock('dotenv');
+import dotenv from 'dotenv';
+
+import { DotEnvLoader } from './DotEnvLoader';
+
+interface MockType {
+  foo: string;
+}
+
+const indexFixture: MockType = { foo: 'bar' };
+
+class DotEnvLoaderMock extends DotEnvLoader<MockType> {
+  protected parseIndex(): MockType {
+    return indexFixture;
+  }
+}
+
+describe(DotEnvLoader.name, () => {
+  let fixturePath: string;
+
+  beforeAll(() => {
+    fixturePath = 'bar';
+  });
+
+  describe('.index', () => {
+    describe('when called', () => {
+      let dotEnvLoader: DotEnvLoader<MockType>;
+      let result: unknown;
+
+      beforeAll(() => {
+        dotEnvLoader = new DotEnvLoaderMock(fixturePath);
+        result = dotEnvLoader.index;
+      });
+
+      it('must return its innerIndex', () => {
+        expect(result).toStrictEqual(indexFixture);
+      });
+    });
+
+    describe('when called, and innerIndex is not defined', () => {
+      let dotEnvLoader: DotEnvLoader<MockType>;
+      let dotEnvLoaderLoadSpy: jest.SpyInstance;
+
+      beforeAll(() => {
+        dotEnvLoader = new DotEnvLoaderMock(fixturePath);
+        dotEnvLoaderLoadSpy = jest.spyOn(dotEnvLoader, 'load');
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        dotEnvLoader.index;
+      });
+
+      it('must call this.load', () => {
+        expect(dotEnvLoaderLoadSpy).toHaveBeenCalledTimes(1);
+      });
+
+      afterAll(() => {
+        dotEnvLoaderLoadSpy.mockClear();
+      });
+    });
+  });
+
+  describe('.load()', () => {
+    let dotEnvLoader: DotEnvLoader<MockType>;
+
+    beforeAll(() => {
+      dotEnvLoader = new DotEnvLoaderMock(fixturePath);
+    });
+
+    describe('when called', () => {
+      beforeAll(() => {
+        (dotenv.config as jest.Mock).mockClear();
+        dotEnvLoader.load();
+      });
+
+      it('must call dontenv.config', () => {
+        expect(dotenv.config).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+});
---
+++ src/env/integration/dotenv/DotEnvLoader.ts
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+
+import { EnvLoader } from '../../domain/EnvLoader';
+
+export abstract class DotEnvLoader<T> implements EnvLoader<T> {
+  protected innerIndex: T | undefined;
+
+  constructor(protected readonly path: string) {
+    this.innerIndex = undefined;
+  }
+
+  public get index(): T {
+    if (undefined === this.innerIndex) {
+      this.load();
+    }
+
+    return this.innerIndex as T;
+  }
+
+  public load(): void {
+    dotenv.config({
+      path: this.path,
+    });
+
+    this.innerIndex = this.parseIndex();
+  }
+
+  protected abstract parseIndex(): T;
+}
