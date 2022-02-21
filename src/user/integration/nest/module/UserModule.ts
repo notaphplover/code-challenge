e9f8bb55c9bcbcd02f1511f20f178e51f228cf23
notaphplover <roberto.pintos.lopez@gmail.com>
2022-02-21 19:54:30 +0100

    refactor(user): update UserModule with FindOneUserManager provider

--- src/user/integration/nest/module/UserModule.ts
+++ src/user/integration/nest/module/UserModule.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { FindOneUserManager } from '../../../domain/manager/FindOneUserManager';
 import { FindUserTypeOrmAdapter } from '../../typeOrm/adapter/FindUserTypeOrmAdapter';
 import { UserFindQueryToUserFindQueryTypeOrmConverter } from '../../typeOrm/converter/UserFindQueryToUserFindQueryTypeOrmConverter';
 import { UserTypeOrmToUserConverter } from '../../typeOrm/converter/UserTypeOrmToUserConverter';
 import { UserTypeOrm } from '../../typeOrm/model/UserTypeOrm';
 
 @Module({
+  exports: [FindOneUserManager],
   imports: [TypeOrmModule.forFeature([UserTypeOrm])],
   providers: [
+    FindOneUserManager,
     FindUserTypeOrmAdapter,
     UserFindQueryToUserFindQueryTypeOrmConverter,
     UserTypeOrmToUserConverter,
