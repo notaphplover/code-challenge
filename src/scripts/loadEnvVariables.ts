fb972ab6b6674ca096446ee3fc3981fe167b0666
notaphplover <roberto.pintos.lopez@gmail.com>
2022-02-21 13:12:54 +0100

    fix: fix loadEnvVariables to rely on AppEnvModule

--- src/scripts/loadEnvVariables.ts
+++ src/scripts/loadEnvVariables.ts
@@ -1,14 +1,14 @@
 import { INestApplicationContext } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from '../app/integration/nest/module/AppModule';
 import { AppEnv } from '../appEnv/domain/model/AppEnv';
 import { AppEnvDotEnvLoader } from '../appEnv/integration/dotenv/adapter/AppEnvDotEnvLoader';
+import { AppEnvModule } from '../appEnv/integration/nest/module/AppEnvModule';
 import { EnvLoader } from '../env/domain/adapter/EnvLoader';
 
 export async function loadEnvVariables(): Promise<void> {
   const nestApplication: INestApplicationContext =
-    await NestFactory.createApplicationContext(AppModule);
+    await NestFactory.createApplicationContext(AppEnvModule);
 
   const appEnvLoader: EnvLoader<AppEnv> =
     nestApplication.get(AppEnvDotEnvLoader);
