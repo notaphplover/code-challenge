50db560801869c7a790e580169f9357d130f15cb
notaphplover <roberto.pintos.lopez@gmail.com>
2022-02-23 01:16:17 +0100

    feat(app): update AppModule with ExerciseModule

--- src/app/integration/nest/module/AppModule.ts
+++ src/app/integration/nest/module/AppModule.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { AppEnv } from '../../../../appEnv/domain/model/AppEnv';
 import { AppEnvDotEnvLoader } from '../../../../appEnv/integration/dotenv/adapter/AppEnvDotEnvLoader';
 import { AppEnvModule } from '../../../../appEnv/integration/nest/module/AppEnvModule';
 import { EnvLoader } from '../../../../env/domain/adapter/EnvLoader';
+import { ExerciseModule } from '../../../../exercise/integration/nest/module/ExerciseModule';
+import { ExerciseTypeOrm } from '../../../../exercise/integration/typeOrm/model/ExerciseTypeOrm';
 import { UserModule } from '../../../../user/integration/nest/module/UserModule';
 import { UserTypeOrm } from '../../../../user/integration/typeOrm/model/UserTypeOrm';
 import { AppController } from '../controller/AppController';
@@ -13,7 +16,7 @@ function typeOrmFactory(appEnvLoader: EnvLoader<AppEnv>): TypeOrmModuleOptions {
   appEnvLoader.load();
 
   return {
-    entities: [UserTypeOrm],
+    entities: [ExerciseTypeOrm, UserTypeOrm],
   };
 }
 
@@ -21,6 +24,13 @@ function typeOrmFactory(appEnvLoader: EnvLoader<AppEnv>): TypeOrmModuleOptions {
   controllers: [AppController],
   imports: [
     AppEnvModule,
+    ExerciseModule,
+    RouterModule.register([
+      {
+        module: ExerciseModule,
+        path: 'exercises',
+      },
+    ]),
     TypeOrmModule.forRootAsync({
       imports: [AppEnvModule],
       inject: [AppEnvDotEnvLoader],
